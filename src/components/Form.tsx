89c9db401e96cbd161de530ddab5ab5e423dc586
Dercio <contact@creedscode.cc>
2022-10-29 15:47:39 +0200

    LFG!

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,4 +1,26 @@
+import { Listbox, Transition } from '@headlessui/react';
+import { CheckIcon, ChevronUpDownIcon } from '@heroicons/react/20/solid';
+import { Fragment, useState } from 'react';
+
+const smartContracts = [
+  {
+    id: 1,
+    unavailable: false,
+    type: 'staking',
+    name: 'Mock Staking',
+    address: '0xAe46E37B5628947aC159F001a847E874521GAA20',
+  },
+  {
+    id: 2,
+    unavailable: false,
+    type: 'escrow',
+    name: 'Mock Escrow',
+    address: '0xAe46E37B5628947aC159F001a847E874521GAA20',
+  },
+];
+
 export default function Content() {
+  const [selectedSmartContract, setSelectedSmartContract] = useState(smartContracts[0]);
   return (
     <div className='bg-white'>
       <main>
@@ -22,10 +44,53 @@ export default function Content() {
                       Choose the smart contract you would like to claim earnings from.
                     </p>
                     <div className='mt-6'>
-                      <select>
-                        <option value='staking'>Mock Staking</option>
-                        <option value='escrow'>Mock Escrow</option>
-                      </select>
+                      <Listbox value={selectedSmartContract} onChange={setSelectedSmartContract}>
+                        <div className='relative mt-1'>
+                          <Listbox.Button className='relative w-full cursor-default rounded-lg bg-white py-2 pl-3 pr-10 text-left shadow-md focus:outline-none focus-visible:border-indigo-500 focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-orange-300 sm:text-sm'>
+                            <span className='block truncate'>{selectedSmartContract.name}</span>
+                            <span className='pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2'>
+                              <ChevronUpDownIcon
+                                className='h-5 w-5 text-gray-400'
+                                aria-hidden='true'
+                              />
+                            </span>
+                          </Listbox.Button>
+                          <Transition
+                            as={Fragment}
+                            leave='transition ease-in duration-100'
+                            leaveFrom='opacity-100'
+                            leaveTo='opacity-0'>
+                            <Listbox.Options className='absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm'>
+                              {smartContracts.map(contract => (
+                                <Listbox.Option
+                                  key={contract.id}
+                                  className={({ active }) =>
+                                    `relative cursor-default select-none py-2 pl-10 pr-4 ${
+                                      active ? 'bg-amber-100 text-amber-900' : 'text-gray-900'
+                                    }`
+                                  }
+                                  value={contract}>
+                                  {({ selected }) => (
+                                    <>
+                                      <span
+                                        className={`block truncate ${
+                                          selected ? 'font-medium' : 'font-normal'
+                                        }`}>
+                                        {contract.name} <p>{contract.address}</p>
+                                      </span>
+                                      {selected ? (
+                                        <span className='absolute inset-y-0 left-0 flex items-center pl-3 text-amber-600'>
+                                          <CheckIcon className='h-5 w-5' aria-hidden='true' />
+                                        </span>
+                                      ) : null}
+                                    </>
+                                  )}
+                                </Listbox.Option>
+                              ))}
+                            </Listbox.Options>
+                          </Transition>
+                        </div>
+                      </Listbox>
                     </div>
                   </label>
                 </div>
