09556483081c520e4af2f4603a95eb0685931cd9
KPomales <kirstenpomales@gmail.com>
2022-10-29 10:01:18 +0100

    Adding form stuff

--- src/pages/Services.tsx
+++ src/pages/Services.tsx
@@ -1,11 +1,12 @@
 import Menu from '../components/Menu';
 import Footer from '../components/Footer';
+import Form from '../components/Form';
 
 function Services() {
   return (
     <>
       <Menu />
-      <h1 className='font-bold text-center py-11'>You are on the Services Pages</h1>
+      <Form />
       <Footer />
     </>
   );
